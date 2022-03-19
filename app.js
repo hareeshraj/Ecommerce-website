87bc08b9ee1e441bb73e059a656f399a4dc428b1
unknown <gokul03062002@gmail.com>
2022-03-19 14:43:06 +0530

    home

--- app.js
+++ app.js
@@ -15,7 +15,21 @@ const usersignupschema={
     password:String
 };
 const usersignup=mongoose.model("usersignup",usersignupschema);
+const adminsignupschema={
+    name:String,
+    mail:String,
+    mobilenumber:Number,
+    password:String
+};
+const adminsignup=mongoose.model("adminsignup",adminsignupschema);
+const adminsignup1=new adminsignup({
+    name:"gokul",
+    mail:"gokul03062002@gmail.com",
+    mobilenumber:9043322155,
+    password:"gokul2002",
 
+});
+//adminsignup1.save();
 app.get("/",function(req,res)
 {
     res.render("login",{});
@@ -56,30 +70,71 @@ app.post("/usersignin",function(req,res)
 {
     const email=req.body.email;
     const password=req.body.password;
+    const checkBox=req.body.admin;
     console.log(email+" "+password);
-    usersignup.find({mail:email},function(err,found)
+    if(checkBox==="admin")
     {
-        if(err)
+        console.log("admin");
+        adminsignup.find({mail:email},function(err,found)
+        {
+            if(err)
         {
             console.log("error");
         }
-        else{
-            console.log(found[0].password);
+        else if(found.length>0){
+            if(found[0].password===password)
+            {
+                console.log("admin login success");
+                res.render("adminhome",{});
+            } 
+            else{
+                res.redirect("/");
+                console.log("failed");
+            }
+            // console.log(found[0].password);
         }
-       if(found[0].password===password)
-       {
-           console.log("login success");
-           res.render("home",{});
-       } 
-       else{
-           res.redirect("/");
-           console.log("failed");
-       }
+        else{
+            res.redirect("/");
+        }
+     
 
+        });
+    }
+    else{
+        console.log("user");
+        usersignup.find({mail:email},function(err,found)
+        {
+            console.log(found.length);
+            if(err)
+            {
+                console.log("error");
+            }
+            else if(found.length>0){
+                if(found[0].password===password)
+                {
+                    console.log("login success");
+                    res.render("home",{});
+                } 
+                else{
+                    res.redirect("/");
+                    console.log("failed");
+                }
+                
+               //console.log(found[0].password);
+            }
+            else
+            {
+                res.redirect("/");
+            }
+         
+    
+    
+        });
+    
+    
+    }
 
-    });
-
-
+   
 });
 app.listen(process.env.PORT || 3000,function()
 {
