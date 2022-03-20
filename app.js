add71b4d678be48de7dceda703cf70ccfd26cc99
unknown <gokul03062002@gmail.com>
2022-03-20 23:46:11 +0530

    card

--- app.js
+++ app.js
@@ -2,11 +2,32 @@ const express=require("express");
 const app=express();
 const bodyparser=require("body-parser");
 const mongoose=require("mongoose");
+const multer=require("multer");
+const fs=require("fs");
 app.use(bodyparser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://19csr044:Gokul2002@cluster0-shard-00-00.dowfo.mongodb.net:27017,cluster0-shard-00-01.dowfo.mongodb.net:27017,cluster0-shard-00-02.dowfo.mongodb.net:27017/the_fashioner?ssl=true&replicaSet=atlas-108jkb-shard-0&authSource=admin&retryWrites=true&w=majority");
+const storage=multer.diskStorage({
+    
+    destination:function(req,file,callback){
+        callback(null,'./public/upload/images');
+    },
+    
+    filename:function(req,file,callback)
+    {
+        callback(null,Date.now()+'-'+file.originalname);
+    }
+});
+
+
+const upload=multer({
+    storage:storage,
+    limits:{
+        fieldSize:1024*1024*10
+    }
+});
 
 const usersignupschema={
     name:String,
@@ -30,6 +51,18 @@ const adminsignup1=new adminsignup({
 
 });
 //adminsignup1.save();
+const productschema={
+    pname:String,
+    desc:String,
+    pgroup:String,
+    size:String,
+    price:Number,
+    file:String,
+    fileExt: String,
+    base64Code: String
+};
+
+const product=mongoose.model("product",productschema);
 app.get("/",function(req,res)
 {
     res.render("login",{});
@@ -85,7 +118,7 @@ app.post("/usersignin",function(req,res)
             if(found[0].password===password)
             {
                 console.log("admin login success");
-                res.render("adminhome",{});
+                res.redirect("/adminhome");
             } 
             else{
                 res.redirect("/");
@@ -136,8 +169,64 @@ app.post("/usersignin",function(req,res)
 
    
 });
+app.get("/adminhome",function(req,res)
+{
+    product.find({},function(err,found)
+    {
+        if(!err)
+        {
+            res.render("adminhome",{detail:found});
+        }
+
+    });
+    
+
+});
+app.post("/addproduct",upload.single('file'),function (req,res)
+{
+    const file=req.file;
+    let temp = fs.readFileSync(file.path);
+    console.log(file.filename);
+    const pname=req.body.productname;
+    const desc =req.body.description;
+    const group=req.body.productgroup;
+    const price=req.body.price;
+    const size=req.body.size;
+  
+    // console.log(pname+" "+desc+" "+group+" "+price+" "+size+" "+file);
+    const productdata=new product({
+        pname:pname,
+        desc:desc,
+        pgroup:group,
+        size:size,
+        price:price,
+        file:file.originalname,
+        fileExt: file.mimetype,
+        base64Code: temp.toString('base64')
+    });
+   
+    productdata.save();
+   res.redirect("/adminhome");
+});
+
 app.listen(process.env.PORT || 3000,function()
 {
     console.log("server started");
 
+});
+
+//delete files
+
+const path = require('path');
+
+const directory = './public/upload/images';
+
+module.exports = fs.readdir(directory, (err, files) => {
+  if (err) throw err;
+
+  for (const file of files) {
+    fs.unlink(path.join(directory, file), err => {
+      if (err) throw err;
+    });
+  }
 });
